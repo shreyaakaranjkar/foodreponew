8c23133cd9ebe944671363245a623de9fcb89d9e
pradipbkamble <pradipbkamble001@gmail.com>
2024-08-22 23:56:38 +0530

    yes

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,14 @@ let onclick=(eve)=>{
 
 
  toggalnav.addEventListener("click",onclick)
+
+
+
+
+//  <div class="row" >
+//                   <div class="col-3"><i class="fa-regular fa-clock"></i> <span>time</span></div>
+//                   <div class="col-3"><i class="fa-brands fa-superpowers mx-1"></i> <span>SUPER EASY</span></div>
+//                   <div class="col-3"> <i class="fa-solid fa-server mx-1"><span>SERVES 1</span></div>
+//                   <div class="col-3"><i class="fa-solid fa-wine-glass mx-1"><span>DRINKS</span></div>
+                  
+//                  </div>
